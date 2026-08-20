4c2790ea6f9e6e4aa29af63152b8c15e8f574435
Kusuma M G <kusumamg07@gmail.com>
2026-08-20 09:11:07 +0530

    basic problem

---
+++ Basic/perfect_no.py
@@ -0,0 +1,15 @@
+class Solution:
+ def isPerfect(self, n):
+    if n <= 1:
+        return False
+
+    total = 1
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            total += i
+
+            if i != n // i:
+                total += n // i
+
+    return total == n
