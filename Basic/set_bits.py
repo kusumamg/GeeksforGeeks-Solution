471069145700722e52cc77253a90e474ada3d6f9
Kusuma M G <kusumamg07@gmail.com>
2026-09-09 20:47:56 +0530

    Add setBits method to count set bits in an integer

---
+++ Basic/set_bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def setBits(self, n):
+        count = 0
+
+        while n > 0:
+            if n % 2 == 1:
+                count += 1
+            n //= 2
+
+        return count
